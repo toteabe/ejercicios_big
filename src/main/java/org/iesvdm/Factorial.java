ae977705aabd1af2d6f1fd6b4362c7c324b00b93
toteabe <toteabe@yahoo.es>
2023-09-25 07:12:19 +0200

    Init

---
+++ src/main/java/org/iesvdm/Factorial.java
@@ -0,0 +1,42 @@
+package org.iesvdm;
+
+import java.math.BigInteger;
+
+public class Factorial {
+
+    private long n;
+    private BigInteger factorial;
+
+    public long getN() {
+        return n;
+    }
+
+    public void setN(long n) {
+        this.n = n;
+    }
+
+    public BigInteger getFactorial() {
+        return factorial;
+    }
+
+    public void setFactorial(BigInteger factorial) {
+        this.factorial = factorial;
+    }
+
+    public Factorial(long n) {
+        if ( n >= 0)
+            this.n = n;
+        else
+            throw new RuntimeException("Valor " + n + " no permitido.");
+    }
+
+    public BigInteger calcula() {
+
+        this.factorial = BigInteger.ONE;
+        for(long i = 1; i <= this.n; i++) {
+            this.factorial = BigInteger.valueOf(i).multiply(this.factorial);
+        }
+        return this.factorial;
+    }
+
+}
---
+++ src/test/java/InteresCompuestoTest.java
@@ -0,0 +1,24 @@
+import org.iesvdm.InteresCompuesto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class InteresCompuestoTest {
+
+    @Test
+    public void cuandoPrincipal1000Interes5porcientoY10peridos() {
+
+        String principal = "1000";
+        int interes = 5;
+        int periodos = 10;
+
+        InteresCompuesto interesCompuesto = new InteresCompuesto(new BigDecimal(principal), new BigDecimal(interes).divide(new BigDecimal(100)), periodos);
+        interesCompuesto.calculaMontoFinal();
+        BigDecimal cantidadFinal = interesCompuesto.getC().setScale(2, RoundingMode.HALF_UP);
+
+        Assertions.assertEquals(new BigDecimal("1628.89"), cantidadFinal);
+    }
+
+}
